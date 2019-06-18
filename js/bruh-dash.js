2809f5b3a1acfe3276ec077608270c9dbc1ff5c5
Andrew Obiano <andrew.obiano@gmail.com>
2019-06-17 19:01:01 -1000

    most of it is done

--- js/bruh-dash.js
+++ js/bruh-dash.js
@@ -154,22 +154,36 @@ global.bruhdash = {
   // removes elements of an array corresponding to the given indices
   pullAt: function (array, arrIndices) {
     let answer = [];
+    let tempIndex = 0;
     for(i=0; i<array.length; i++){
-      if(array.length === arrIndices[i]){ //IM STUCK HERE
+      if(i === arrIndices[tempIndex]){
         answer.push(array[i]);
+        tempIndex++;
       }
     }
     return answer;
   },
 
   // creates an array excluding all the specified values
-  without: function() {
-
+  without: function(array, val1, val2) {
+    let answer = [];
+    for(i=0; i<array.length; i++){
+      if(array[i] !== val1 && array[i] !== val2){
+        answer.push(array[i]);
+      }
+    }
+    return answer;
   },
 
   // returns an array with specified values excluded
-  difference: function() {
-
+  difference: function(array1, array2) {
+    let answer = [];
+    for(i=0; i<array1.length; i++){
+      if(array1[i] !== array2[i]){
+        answer.push(array1[i]);
+      }
+    }
+    return answer;
   },
 
   /*******************
@@ -177,18 +191,44 @@ global.bruhdash = {
    *******************/ 
 
   // creates an array of grouped elements
-  zip: function () {
-
+  zip: function (array1, array2) {
+    let answer = [];
+    for(i=0; i<array1.length; i++){
+      let tempArr = [];
+      tempArr.push(array1[i]);
+      tempArr.push(array2[i]);
+      answer.push(tempArr);
+    }
+    return answer;
   },
 
   // creates an array of grouped elements in their pre-zip configuration
-  unzip: function () {
-
+  unzip: function (array) {
+    let answer = [];
+    let tempArr1 = [];
+    let tempArr2 = [];
+    for(i=0; i<array.length; i++){
+      tempArr1.push(array[i][0]);
+      tempArr2.push(array[i][1]);
+    }
+    answer.push(tempArr1);
+    answer.push(tempArr2);
+    return answer;
   },
 
   // creates an array of elements into groups of length of specified size
-  chunk: function(){
-
+  chunk: function(array, val){ // IM STUCK
+    let emptyArr = [];
+    if(val === 0){
+      return emptyArr;
+    }else if(val === array.length || val >= array.length){
+      emptyArr.push(array);
+      return emptyArr;
+    }else if(array && val){
+      
+    }else{
+      return emptyArr;
+    }
   },
 
   // iterates over elements of a collection and invokes iteratee for each element
